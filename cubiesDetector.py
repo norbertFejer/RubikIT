a701773f0f0238d258f007431eb8a71ecfa0a51f
Norbert Fejer <norbertfejer@protonmail.com>
2021-06-08 01:40:45 +0300

    Object detection algorithm updated

--- cubiesDetector.py
+++ cubiesDetector.py
@@ -12,6 +12,8 @@ from numpy.lib.type_check import imag
 import cv2
 from imutils import contours
 
+import sys
+
 
 class ArrowDirection(Enum):
     LEFT = 0
@@ -58,7 +60,7 @@ class ImageProcessor:
 
 
     def __dilatate_edges (self, edges, iter_num = 3):
-        kernel = self.__create_new_kernel (5, 5)
+        kernel = self.__create_new_kernel (3, 3)
         return cv2.dilate(edges, kernel, iterations = iter_num)
 
     
@@ -67,21 +69,22 @@ class ImageProcessor:
         return raw_contours
 
 
-    def __get_four_corners (self, contours, max_accepted_area):
+    def __get_four_corners (self, contours, max_accepted_area, min_accepted_area):
         four_corners = []
-        four_corners_area_sum = 0
         for cnt in contours:
-            epsilon = 0.1 * cv2.arcLength(cnt, True)
+            epsilon = 0.08 * cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, epsilon, True)
 
             if 4 == len(approx):
-                area = cv2.contourArea(cnt)
 
-                if area < max_accepted_area:
+                (x, y, w, h) = cv2.boundingRect(approx)
+                #aspect_ratio = w / float(h)
+
+                # aspect_ratio >= 0.8 and aspect_ratio <= 1.2
+                if (w*h < max_accepted_area) and (w*h > min_accepted_area):
                     four_corners.append(cnt)
-                    four_corners_area_sum += area
 
-        return four_corners, four_corners_area_sum
+        return four_corners
 
 
     def __get_cube_pieces (self, four_corners, treshold_area):
@@ -125,22 +128,140 @@ class ImageProcessor:
 
 
     def get_contours (self, image):
-        original_hsv = self.__convert_to_hsv (image)
+        #original_hsv = self.__convert_to_hsv (image)
+        mask = cv2.imread('C:/MyDocs/University/SoftwareSystemModelling/Rubik_cube_solver/samples/mask.png')
+        image = cv2.bitwise_and(image, mask)
 
-        edges = self.__get_edges_from_image (original_hsv, 320, 20)
-        dilation = self.__dilatate_edges (edges)
+        grayscale = self.__convert_to_grayscale (image)
+
+        grayscale = cv2.GaussianBlur(grayscale,(9,3),3)
+
+        laplacian = cv2.Laplacian(grayscale, cv2.CV_16S, ksize=5)
+        laplacian = cv2.convertScaleAbs(laplacian)
+
+        edges = self.__get_edges_from_image (laplacian, 120, 240)
+        dilation = self.__dilatate_edges (edges, 3)
+
+        #cv2.imshow('RubikIT', dilation)
+        #cv2.waitKey (0)
+
+        #laplacian = laplacian.astype(np.uint8)
+        #laplacian=(laplacian-laplacian.min())/(laplacian.max()-laplacian.min())
+
+
+        #canny1 = cv2.Canny(np.uint8(laplacian* 255), 320, 20)
 
         raw_contours = self.__get_contours_from_image (dilation)
-        original_img_area = image.shape[0] * image.shape[1]
-        max_accepted_area = original_img_area / 3
-        four_corners, four_corners_area_sum =  self.__get_four_corners (raw_contours, max_accepted_area)
+        #print(len(raw_contours))
 
-        avg_area_of_four_corners = four_corners_area_sum / len(four_corners)
-        extracted_edges = self.__get_cube_pieces (four_corners, avg_area_of_four_corners)
+        max_accepted_area = int(image.shape[0] / 3) * int(image.shape[0] / 3)
+        min_accepted_area = int(image.shape[0] / 16) * int(image.shape[0] / 16)
 
-        sorted_contours = self.__get_sorted_contours (extracted_edges)
+        four_corners = self.__get_four_corners (raw_contours, max_accepted_area, min_accepted_area)
+        #print(len(four_corners), 'four_corners...')
 
-        return self.__get_approximated_rectangles_from_abstract_contours (sorted_contours)
+        detected_cubies_contour = self.get_valid_cubies_from_contours (four_corners)
+        if len(detected_cubies_contour) == 0:
+             return []
+
+        detected_cubies_contour = self.__get_sorted_contours (detected_cubies_contour)
+
+        if len(detected_cubies_contour) == 9:
+            return detected_cubies_contour
+        
+        return []
+
+    
+    def get_contour_max_boundary_coordinates (self, contour):
+        max_x, max_y, min_x, min_y = 0, 0, sys.maxsize, sys.maxsize
+        for cnt in contour:
+            actual_point_x = cnt[0][0]
+            actual_point_y = cnt[0][1]
+            
+            if actual_point_x > max_x:
+                max_x = actual_point_x
+            elif actual_point_x < min_x:
+                min_x = actual_point_x
+
+            if actual_point_y > max_y:
+                max_y = actual_point_y
+            elif actual_point_y < min_y:
+                min_y = actual_point_y
+
+        return (min_x, min_y), (max_x, max_y)
+
+
+    def is_points_in_same_interval (self, point1, point2, tolerance=30):
+
+        if (((point2[0] < point1[0] + tolerance) and 
+                (point2[0] > point1[0] - tolerance)) or
+                ((point2[1] < point1[1] + tolerance) and
+                (point2[1] > point1[1] - tolerance))):
+            return True
+        return False
+
+
+    def get_valid_cubies_from_contours (self, contours):
+
+        cubie_contours = []
+        for actual_cnt in contours:
+            start_coord_actual_cnt, end_coord_actual_cnt = self.get_contour_max_boundary_coordinates (actual_cnt)
+            actual_ctn_area = cv2.contourArea(actual_cnt)
+
+            match_x_coord_count = 0
+            match_y_coord_count = 0
+            for cnt in contours:
+                start_coord_other_cnt, end_coord_other_cnt = self.get_contour_max_boundary_coordinates (cnt)
+
+                # checking if we selected the same contour as the actual one
+                if ((start_coord_actual_cnt[0] == start_coord_other_cnt[0]) and
+                        (start_coord_actual_cnt[1] == start_coord_other_cnt[1]) and
+                        (end_coord_actual_cnt[0] == end_coord_other_cnt[0]) and
+                        (end_coord_actual_cnt[1] == end_coord_other_cnt[1])):
+                    continue
+
+                if self.is_points_in_same_interval (start_coord_actual_cnt, start_coord_other_cnt):
+                    #other_ctn_area = cv2.contourArea(cnt)
+                    #if (actual_ctn_area >= other_ctn_area * 0.8) and (actual_ctn_area <= other_ctn_area * 1.2):
+                    match_x_coord_count += 1
+                
+                if self.is_points_in_same_interval (end_coord_actual_cnt, end_coord_other_cnt):
+                    #other_ctn_area = cv2.contourArea(cnt)
+                    #if (actual_ctn_area >= other_ctn_area * 0.8) and (actual_ctn_area <= other_ctn_area * 1.2):
+                    match_y_coord_count += 1
+
+            if match_x_coord_count >= 4 and match_y_coord_count >= 4:
+                cubie_contours.append (actual_cnt)
+
+        return cubie_contours
+
+
+    def __is_all_countour_a_cubie (self, contours):
+        
+        tolerance = 30
+        start_coord_1, end_coord_1 = self.get_contour_max_boundary_coordinates (contours[0])
+        start_coord_2, end_coord_2 = self.get_contour_max_boundary_coordinates (contours[4])
+        if not self.is_points_in_same_interval (end_coord_1, start_coord_2, tolerance):
+            return False
+
+        start_coord_1, end_coord_1 = self.get_contour_max_boundary_coordinates (contours[4])
+        start_coord_2, end_coord_2 = self.get_contour_max_boundary_coordinates (contours[8])
+        if not self.is_points_in_same_interval (end_coord_1, start_coord_2, tolerance):
+            return False
+
+        start_coord_1, end_coord_1 = self.get_contour_max_boundary_coordinates (contours[3])
+        start_coord_2, end_coord_2 = self.get_contour_max_boundary_coordinates (contours[7])
+        if not self.is_points_in_same_interval (end_coord_1, start_coord_2, tolerance):
+            return False
+
+        start_coord_1, end_coord_1 = self.get_contour_max_boundary_coordinates (contours[1])
+        start_coord_2, end_coord_2 = self.get_contour_max_boundary_coordinates (contours[5])
+        if not self.is_points_in_same_interval (end_coord_1, start_coord_2, tolerance):
+            return False
+
+        return True
+
+        
 
 
 class ImageDrawer:
@@ -227,6 +348,10 @@ class ImageDrawer:
 
     def show_image (self, image, image_name=''):
         cv2.imshow(image_name, image)
+
+    
+    def draw_rectangle (self, image, start_point, end_point, color=(255, 0, 0), thickness=9):
+        cv2.rectangle(image, start_point, end_point, color, thickness)
             
 
 class ColorMapper:
@@ -312,30 +437,45 @@ class VideoProcessor:
 
         cubie_contours = self._imageProcessor.get_contours (frame)
         self._imageDrawer.draw_contours (frame, cubie_contours)
+        #self._imageDrawer.draw_rectangle (frame, (170, 100), (470, 500))
         cv2.imshow('RubikIT', frame)
 
 
+
+def test_1 ():
+    imageLoader = ImageLoader()
+    imageProcessor = ImageProcessor ()
+    imageDrawer = ImageDrawer ()
+
+    image = imageLoader.load_image ('C:/MyDocs/University/SoftwareSystemModelling/Rubik_cube_solver/samples', 'real.png')
+    image = imageProcessor.resize_image (image, 640, 640)
+
+    cubie_contours = imageProcessor.get_contours (image)
+    print(len(cubie_contours), 'original contour len')
+    cubies = imageProcessor.get_valid_cubies_from_contours (cubie_contours)
+    print(len(cubies), 'cubies len')
+
+    # p1 = (300, 300)
+    # p2 = (0, 360)
+    # print('egy intervallumban:', imageProcessor.is_points_in_same_interval(p1, p2))
+
+
+    imageDrawer.draw_contours (image, cubie_contours)
+    imageDrawer.show_image (image)
+
+
+def test_2 ():
+    videoProcessor = VideoProcessor(1, 640, 640)
+    video_producer_thread = Thread(target = videoProcessor.start_processing_video ())
+    video_producer_thread.start()
 
 
 
 def main ():
 
-    imageLoader = ImageLoader()
-    imageProcessor = ImageProcessor ()
-    imageDrawer = ImageDrawer ()
-
-    image = imageLoader.load_image ('C:/MyDocs/University/SoftwareSystemModelling/Rubik_cube_solver/samples', 'cube02.png')
-    image = imageProcessor.resize_image (image, 615, 820)
-
-    cubie_contours = imageProcessor.get_contours (image)
-
-    # # imageDrawer.draw_arrow_to_all_row (image, cubie_contours, ArrowDirection.RIGHT)
-    imageDrawer.draw_contours (image, cubie_contours)
-    imageDrawer.show_image (image)
-
-    #videoProcessor = VideoProcessor(0, 540, 540)
-    #video_producer_thread = Thread(target = videoProcessor.start_processing_video ())
-    #video_producer_thread.start()
+    #test_1 ()
+    test_2 ()
+    
 
     cv2.waitKey (0)
     cv2.destroyAllWindows()
